800eb0c4e2817b4bf075f73089964a812c6024bb
Ritika-pal38 <palritika294@gmail.com>
2025-03-07 10:45:38 +0530

    [Ritika] Added code for pincode UC3

---
+++ pincode/UC3.java
@@ -0,0 +1,20 @@
+package org.bridgelabz.regex.pincode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UC3 {
+    public static void main(String[] args) {
+        String pinCode = "400088A";
+        String regex = "^[0-9]{6}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pinCode);
+
+        if (matcher.matches()) {
+            System.out.println(pinCode + " is a valid PIN code.");
+        } else {
+            System.out.println(pinCode + " is an invalid PIN code.");
+        }
+    }
+}
